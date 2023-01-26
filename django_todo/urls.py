7b91cef9c6db8e58a0e351a4e17bff092106fc7c
Tinks18 <tinutani9@gmail.com>
2023-01-26 15:44:49 +0000

    Updating an item

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, add_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add'),
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
 ]
+
+
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -14,10 +15,33 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
+        form  = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+            'form': form
+            }
+    return render(request, 'todo/add_item.html', context)
 
-        return redirect('get_todo_list')
+def edit_item(request,item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form=ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)    
+    context = {
+            'form': form
+            }
+    return render(request, 'todo/edit_item.html', context)
 
-    return render(request, 'todo/add_item.html')
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
