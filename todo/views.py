21c3c9ac36e0ef503cd59016fef6901f17b4c530
Tinks18 <tinutani9@gmail.com>
2023-01-26 15:49:28 +0000

    Deleting an item

--- todo/views.py
+++ todo/views.py
@@ -44,4 +44,11 @@ def toggle_item(request, item_id):
     item.save()
     return redirect('get_todo_list')
 
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
+
+
+
 
