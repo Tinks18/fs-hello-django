466a18caa753a303cc727d954742806ef5674b61
Tinks18 <tinutani9@gmail.com>
2023-01-26 19:41:16 +0000

    security

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+if os.path.exists("env.py")
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- todo/views.py
+++ todo/views.py
@@ -15,7 +15,7 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        form  = ItemForm(request.POST)
+        form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
@@ -25,10 +25,11 @@ def add_item(request):
             }
     return render(request, 'todo/add_item.html', context)
 
-def edit_item(request,item_id):
+
+def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
-        form=ItemForm(request.POST, instance=item)
+        form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
@@ -38,11 +39,13 @@ def edit_item(request,item_id):
             }
     return render(request, 'todo/edit_item.html', context)
 
+
 def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
     return redirect('get_todo_list')
+
 
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
